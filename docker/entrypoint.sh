7b32ebd61511aa84fecf71ea5bd473aab0b87730
Artem Shishkin <verter25@gmail.com>
2025-05-27 22:37:23 +0400

    Readme upd

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -19,5 +19,10 @@ if [ ! -d ".venv" ]; then
 fi
 source .venv/bin/activate
 
+# --------------- PATCH: increase startup_timeout for hivemind ----------------
+# This ensures the DHT daemon has more time to start (fixes some network errors)
+sed -i -E 's/(startup_timeout: *float *= *)[0-9.]+/\1120/' .venv/lib/python3.10/site-packages/hivemind/p2p/p2p_daemon.py || true
+
+
 # ----- auto-reply to interactive setup -----
 printf "Y\nA\n0.5\nN\n" | ./run_rl_swarm.sh 
